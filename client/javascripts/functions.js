30c1d2f2c829c4fb58d412ba983fd8046430b59d
River Lane Weston <river.weston123@gmail.com>
2020-04-22 15:15:21 -0700

    Step 3 complete: Coding Practice

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,54 @@
 // jshint esversion: 6
 
+//Req. 3-a
+
+//easy way
+let revWords1 = (str) => {
+    let char = str.split(" ");
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+      result = str[i] + result;
+    }
+    return result;
+}
+/*
+//hard way
+let revWords1 = (str) => {
+    let words = str.split(" ");
+    let temp;
+    for (let i = 0; i < words.length; i++) {
+        temp = "";
+        for (let j = words[i].length-1; j >= 0; j--) {
+            temp += words[i][j];
+        }
+        words[i] = temp;
+    }
+    return words.join(" ");
+}
+*/
+
+//Req. 3-b
+let revWords2 = (str) => {
+    let arr = str.split('');
+    let result = "";
+
+    arr.forEach((char) => {
+      result = char + result;
+    });
+    return result;
+};
+
+//Req. 3-c
+let revWords3 = (str) => {
+    let result = "";
+
+  for(char of str){
+      result = char + result;
+    };
+    return result;
+};
+
+//extra credit code
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
